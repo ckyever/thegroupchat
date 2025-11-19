aee5912d035cf1febed65bdd93d24dd218ac6eb2
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-19 14:31:51 +1100

    Setup routers

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import path from "node:path";
 
+import { indexRouter } from "./routes/indexRouter.js";
+
 const currentDirectory = process.cwd();
 const app = express();
 
 app.set("views", path.join(currentDirectory, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", indexRouter);
 
 const PORT = 3000;
 
