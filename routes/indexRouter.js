291356ee8ba55900909408752268328cdb0ef79a
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-19 16:38:46 +1100

    Show messages on index page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 
+const messages = [
+  {
+    text: "First!",
+    user: "ckyzo",
+    added: new Date(),
+  },
+  {
+    text: "Second?!",
+    user: "chanpan",
+    added: new Date(),
+  },
+];
+
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { messages: messages });
 });
 
 export { indexRouter };
